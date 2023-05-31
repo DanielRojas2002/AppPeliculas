7765d8ac06842456523c367a282ccff26a78c1ce
danym <danymessi1000@gmail.com>
2023-05-30 20:04:03 -0600

    cambio peliculas

---
+++ Controllers/PeliculaController.cs
@@ -0,0 +1,104 @@
+﻿using AppPeliculas.Models;
+using AppPeliculas.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppPeliculas.Controllers
+{
+    public class PeliculaController : Controller
+    {
+
+        private readonly DbpeliculasContext _context;
+
+        public PeliculaController(DbpeliculasContext context)
+        {
+            _context = context;
+        }
+
+
+
+        public IActionResult Index()
+        {
+            var datos = _context.Peliculas.Include(m => m.IdCategoriaNavigation).Include(d => d.IdEstatusPeliculaNavigation);
+
+            return View(datos.ToList());
+        }
+
+        public IActionResult Upsert(int? idpelicula)
+        {
+            PeliculaVM peliculas = new PeliculaVM()
+            {
+                pelicula = new Pelicula(),
+
+                ListaCateogoria = _context.Categoria.Select(m => new SelectListItem
+                {
+                    Text = m.Descripcion,
+                    Value = m.IdCategoria.ToString()
+                }),
+
+                ListaEstatus = _context.EstatusPeliculas.Select(a=> new SelectListItem
+                {
+                    Text=a.Descripcion.ToString(),
+                    Value=a.IdEstatusPelicula.ToString()
+                })
+
+            };
+
+            if (idpelicula == null)
+            {
+                //crear
+
+                peliculas.pelicula.IdPelicula = 0;
+
+                return View(peliculas);
+
+            }
+            else
+            {
+                //editar
+
+                peliculas.pelicula = _context.Peliculas.Find(idpelicula);
+                return View(peliculas);
+            }
+
+            
+          
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(PeliculaVM modelo)
+        {
+            if (modelo.pelicula.IdPelicula==0)
+            {
+                // crear
+
+                //string horaFormateada = modelo.pelicula.Duracion.HasValue
+                //    ? modelo.pelicula.Duracion.Value.ToString("HH:mm")
+                //    : string.Empty;
+
+
+                Pelicula pelicula = new Pelicula()
+                {
+                    IdCategoria = modelo.pelicula.IdCategoria,
+                    IdEstatusPelicula = modelo.pelicula.IdEstatusPelicula,
+                    Titulo = modelo.pelicula.Titulo.ToUpper(),
+                    Descripcion = modelo.pelicula.Descripcion.ToLower(),
+                    Duracion = modelo.pelicula.Duracion,
+                    FechaRegistro= DateTime.Now
+
+                };
+
+                _context.Peliculas.Add(pelicula);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+            }
+            else
+            {
+                //editar
+            }
+            return View();
+        }
+    }
+}
---
+++ Models/ViewModels/PeliculaVM.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AppPeliculas.Models.ViewModels
+{
+    public class PeliculaVM
+    {
+        public Pelicula pelicula { get; set; }
+
+        public IEnumerable<SelectListItem> ListaCateogoria  { get; set; }
+
+        public IEnumerable<SelectListItem> ListaEstatus { get; set; }
+    }
+}
